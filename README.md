2729296f26e14239a7e194b3c11e659fac772517
danieleBattistelli <bomber81@gmail.com>
2024-11-19 22:21:07 +0100

    feat: readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Descrizione
+### Esercizio
 
 **Milestone 1**
 Sfruttando gli screen e gli asset in allegato riproduciamo la grafica proposta in maniera statica: utilizzando soltanto HTML e CSS e riproducendo una singola fotografia (usiamo una qualunque immagine a piacimento)
@@ -10,3 +10,24 @@ Studiamo bene la risposta e i dati che ci fornisce iniziando a pensare a come po
 Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API di JSON Placeholder, sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
 **Bonus**
 rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano man mano una sotto l’altra ed il titolo abbia una dimensione adeguata
+
+### dati
+endpoint  di JSON Placeholder ;
+
+#### Esecuzione Logica
+Definisco variabile per modificare l'HTML 
+
+Uso axios per fare la chiamata get all'endpoint. Che in questo caso , tramite query string ?_limit=6 , viene limitato a 6 elementi.
+
+Uso then per interagire con la risposta della chiamata.
+
+Salvo la risposta in una variabile e per pogni elemento 
+(tramite un foreach) creo un div inserendoclasse formata da colonne variabili per ottenre una pagina responsive e infine vado ad inserire i dati nell ' html.
+
+Aggiungo le modifiche all'HTML tramite appenchild
+
+Infine gestisco eventuali errori tramite catch per segnalarlo nel console log
+
+### Output
+
+Visualizzo le photocard nella pagina. 
--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 
-    let photoGallery =document.getElementById('photo-gallery');
-    axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
-      .then(response => {
-        const photos = response.data;
+//definisco variabile per modificare HTML
+let photoGallery = document.getElementById('photo-gallery');
+//console.log(photoGallery)
+
+//uso axios per fare la chiamata get all'endpoint
+axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+    //uso then per interagire con la risposta della chiamata 
+    .then((resp) => {
+        const photos = resp.data;
+        //console.table(photos)
         photos.forEach(photo => {
-          const photoCard = document.createElement('div');
-          photoCard.className = 'col-12 col-md-6 col-lg-4 photo-card';
-          photoCard.innerHTML = `
-            <div class="card" id="card">
-              <img src="${photo.url}" class="card-img-top" alt="${photo.title}">
-              <div class="card-body">
+            const photoCard = document.createElement('div');
+            photoCard.className = 'col-12 col-md-6 col-lg-4 photo-card';
+            photoCard.innerHTML = `
+             <div class="card" id="card">
+             
+               <img src="${photo.url}" class="card-img-top" alt="${photo.title}">
+               <div class="card-body">
                 <h4 class="card-title">${photo.title}</h4>
-              </div>
-            </div>
+               </div>
+             </div>
           `;
-          photoGallery.appendChild(photoCard);
+            photoGallery.appendChild(photoCard);
+            console.log(photoCard)
         });
-      })
-      .catch(error => console.error('Error fetching photos:', error));
-  
-  
+
+    })
+     .catch(error=> console.error ('errore nel caricamento delle foto' , error));
